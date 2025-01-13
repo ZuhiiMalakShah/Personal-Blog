4fbb0aa48e79cf2a383da1482e8baf41fe3277ca
Zuhii mz <165068338+ZuhiiMalakShah@users.noreply.github.com>
2025-01-14 00:15:15 +0500

    Update page.tsx

--- src/app/(routs)/portfolio/page.tsx
+++ src/app/(routs)/portfolio/page.tsx
@@ -83,10 +83,10 @@ export default function Portfolio() {
 
                         <div className="flex justify-center space-x-8 bg-[#FFB200] p-3 rounded-lg shadow-lg">
                         <p className="text-2xl font-bold text-black">Check :</p>
-                            <Link href="https://sikandar-folio.vercel.app/" target="_blank" rel="noopener noreferrer">
+                            <Link href="" target="_blank" rel="noopener noreferrer">
                                 <SiVercel className="text-black text-3xl hover:text-gray-600 bg-gray-100 p-2 rounded-md transition-transform transform hover:scale-110" />
                             </Link>
-                            <Link href="https://github.com/SikandarTahir23/Porto" target="_blank" rel="noopener noreferrer">
+                            <Link href="https://github.com/ZuhiiMalakShah/My_portfolio_with_Zuhii_Shah" target="_blank" rel="noopener noreferrer">
                                 <FaGithub className="text-black text-3xl hover:text-gray-600 bg-gray-100 p-2 rounded-md transition-transform transform hover:scale-110" />
                             </Link>
                         </div>
