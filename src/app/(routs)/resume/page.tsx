44eab78e7a3df1d0dbab8c9d2864aed2b2c39f37
Zuhii mz <165068338+ZuhiiMalakShah@users.noreply.github.com>
2025-01-14 00:30:37 +0500

    Update page.tsx

--- src/app/(routs)/resume/page.tsx
+++ src/app/(routs)/resume/page.tsx
@@ -76,10 +76,10 @@ export default function Resume() {
 
                         <div className="flex justify-center space-x-8 bg-[#FFB200] p-3 rounded-lg shadow-lg">
                         <p className="text-2xl font-bold text-black">Check :</p>
-                            <Link href="https://sikandar-resume.vercel.app/" target="_blank" rel="noopener noreferrer">
+                            <Link href="https://milestone-3-dynamic-resume-builder-mu.vercel.app/" target="_blank" rel="noopener noreferrer">
                                 <SiVercel className="text-black text-3xl hover:text-gray-600 bg-gray-100 p-2 rounded-md transition duration-300" />
                             </Link>
-                            <Link href="https://github.com/SikandarTahir23/Hackathon" target="_blank" rel="noopener noreferrer">
+                            <Link href="https://github.com/ZuhiiMalakShah/milestone-1-Static-Resume-Builder" target="_blank" rel="noopener noreferrer">
                                 <FaGithub className="text-black text-3xl hover:text-gray-600 bg-gray-100 p-2 rounded-md transition duration-300" />
                             </Link>
                            
